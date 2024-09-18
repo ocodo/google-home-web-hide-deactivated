5e57533bc66e77cd93bf7272497c491968726f84
jasonm23 <jasonm23@gmail.com>
2024-09-18 18:30:54 +0700

    Run hide deactivated cards for each DOM mutation

--- content.js
+++ content.js
@@ -1,7 +1,21 @@
-document.querySelectorAll('mat-card').forEach(card => {
-    const description = card.querySelector('.description');
-    if (description && description.textContent.includes('Deactivated ')) {
-        card.style.display = 'none';
-    }
-});
+function hideDeactivatedCards() {
+    document.querySelectorAll('mat-card').forEach(card => {
+        const description = card.querySelector('.description');
+        if (description && description.textContent.includes('Deactivated ')) {
+            card.style.display = 'none';
+        }
+    });
+}
 
+function observe(mutation) {
+    mutation.forEach(hideDeactivatedCards)
+}
+
+hideDeactivatedCards()
+
+const observer = new MutationObserver(observe)
+
+observer.observe(document.body), {
+    childList: true,
+    subtree: true
+})
